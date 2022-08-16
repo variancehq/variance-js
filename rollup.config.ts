f0dc2b06841b6c49e30763b99e7f00b73cc865f4
Guillaume Mathieu <contact@gmathieu.com>
2022-08-16 14:22:34 -0700

    build: inject entry point + cjs

---
+++ rollup.config.ts
@@ -0,0 +1,39 @@
+import replace from '@rollup/plugin-replace'
+import typescript from '@rollup/plugin-typescript'
+import type { RollupOptions } from 'rollup'
+
+import { version } from './package.json'
+
+function external(source: string, _importer: unknown, isResolved: boolean) {
+  return !isResolved && !source.startsWith('.')
+}
+
+const replacePlugin = replace({
+  preventAssignment: true,
+  values: {
+    VERSION: JSON.stringify(version),
+  },
+})
+
+const config: RollupOptions = {
+  external,
+  input: ['src/index.ts', 'src/inject.ts'],
+  output: [
+    {
+      dir: 'dist/esm',
+      format: 'es',
+      sourcemap: true,
+      sourcemapExcludeSources: true,
+    },
+    {
+      dir: 'dist/cjs',
+      entryFileNames: '[name].cjs',
+      format: 'cjs',
+      preserveModules: true,
+      sourcemap: true,
+      sourcemapExcludeSources: true,
+    },
+  ],
+  plugins: [replacePlugin, typescript()],
+}
+export default config
